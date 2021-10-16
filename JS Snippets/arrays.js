c1d2ce43eb1211f7869567de11e4e5006f09f219
Akash Roshan <akash.second1999@gmail.com>
2021-10-16 10:50:46 +0530

    complete section 10

--- JS Snippets/arrays.js
+++ JS Snippets/arrays.js
@@ -21,16 +21,32 @@ function fizzBuzz(limit) {
 }
 fizzBuzz(100);
 
-// FizzBuzz problem
+// FizzBuzz problem (while loop)
 
 var output = [];
 var count = 1;
 function fizzBuzz() {
-	if (count % 3 == 0 && count % 5) output.push("FizzBuzz");
-	else if (count % 3 == 0) output.push("Fizz");
-	else if (count % 5 == 0) output.push("Buzz");
-	else output.push(count);
-	count++;
+	while (count <= 100) {
+		if (count % 3 == 0 && count % 5) output.push("FizzBuzz");
+		else if (count % 3 == 0) output.push("Fizz");
+		else if (count % 5 == 0) output.push("Buzz");
+		else output.push(count);
+		count++;
+	}
+	console.log(output);
+}
+fizzBuzz();
+
+// FizzBuzz problem (for loop)
+
+var output = [];
+function fizzBuzz() {
+	for (let index = 1; index <= 10; index++) {
+		if (index % 3 == 0 && index % 5) output.push("FizzBuzz");
+		else if (index % 3 == 0) output.push("Fizz");
+		else if (index % 5 == 0) output.push("Buzz");
+		else output.push(index);
+	}
 	console.log(output);
 }
 fizzBuzz();
--- JS Snippets/intermediateChallenges.js
+++ JS Snippets/intermediateChallenges.js
@@ -26,3 +26,41 @@ function whosPaying(names) {
 	var randomPerson = Math.floor(Math.random() * names.length);
 	return names[randomPerson] + " is going to buy lunch today!";
 }
+
+// 99 Bottles Challenge
+
+function bottles99() {
+	var bottles = 99;
+	while (bottles > 0) {
+		if (bottles == 1) {
+			console.log(
+				"1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
+			);
+			console.log(
+				"No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+			);
+		}
+		console.log(
+			bottles +
+				" bottles of beer on the wall, " +
+				bottles +
+				" bottles of beer. Take one down and pass it around, " +
+				--bottles +
+				" bottles of beer on the wall."
+		);
+	}
+}
+bottles99();
+
+// Fibonacci Excercise
+
+function fibonacciGenerator(n) {
+	var output;
+	if (n == 1) output = [0];
+	else {
+		output = [0, 1];
+		for (let index = 2; index < n; index++)
+			output.push(output[index - 1] + output[index - 2]);
+	}
+	return output;
+}
