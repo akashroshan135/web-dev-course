bc88ebf4f9bfdee7b1357c06b70af314400c3c48
Akash Roshan <akash.second1999@gmail.com>
2021-10-16 11:25:25 +0530

    complete section 11

--- DOM/index.js
+++ DOM/index.js
@@ -1,2 +1,5 @@
-alert("Hello");
-document.querySelector("h1").innerHTML = "Good Bye";
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Akash";
+// document.querySelector("li a").style.color = "red";
+// document.querySelector("button").style.backgroundColor = "red";
+// document.querySelector("h1").classList.add("huge");
+// document.querySelector("a").setAttribute("href", "https://www.bing.com/");
