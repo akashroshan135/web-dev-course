558acc4b420e5fb12a8706b9d5f2add6aaa3dac6
Akash Roshan <akash.second1999@gmail.com>
2021-10-18 10:52:34 +0530

    lecture 170 keyboard listeners

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,29 +1,42 @@
+// Calls drum function based on the button clicked
 document.querySelectorAll(".drum").forEach((button) => {
 	button.addEventListener("click", function () {
-		var audio;
-		switch (this.innerHTML) {
-			case "w":
-				audio = new Audio("sounds/tom-1.mp3");
-				break;
-			case "a":
-				audio = new Audio("sounds/tom-2.mp3");
-				break;
-			case "s":
-				audio = new Audio("sounds/tom-3.mp3");
-				break;
-			case "d":
-				audio = new Audio("sounds/tom-4.mp3");
-				break;
-			case "j":
-				audio = new Audio("sounds/snare.mp3");
-				break;
-			case "k":
-				audio = new Audio("sounds/crash.mp3");
-				break;
-			case "l":
-				audio = new Audio("sounds/kick-bass.mp3");
-				break;
-		}
-		audio.play();
+		playDrum(this.innerHTML);
 	});
 });
+
+// Calls drum function based on the key pressed
+document.addEventListener("keydown", function (event) {
+	playDrum(event.key);
+});
+
+// Plays drums
+function playDrum(key) {
+	var audio;
+	switch (key) {
+		case "w":
+			audio = new Audio("sounds/tom-1.mp3");
+			break;
+		case "a":
+			audio = new Audio("sounds/tom-2.mp3");
+			break;
+		case "s":
+			audio = new Audio("sounds/tom-3.mp3");
+			break;
+		case "d":
+			audio = new Audio("sounds/tom-4.mp3");
+			break;
+		case "j":
+			audio = new Audio("sounds/snare.mp3");
+			break;
+		case "k":
+			audio = new Audio("sounds/crash.mp3");
+			break;
+		case "l":
+			audio = new Audio("sounds/kick-bass.mp3");
+			break;
+		default:
+			return;
+	}
+	audio.play();
+}
