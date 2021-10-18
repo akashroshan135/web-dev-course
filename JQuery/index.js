d847d8695509342352250a1ebe68d9926d79e43e
Akash Roshan <akash.second1999@gmail.com>
2021-10-18 11:40:12 +0530

    lecture 181

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,9 +1,11 @@
-$(document).ready(function () {
-	// $("h1").css("color", "red");
-	$("h1").addClass("big-title margin-50");
-	// $("h1").removeClass("big-title");
-	// $("h1").hasClass("margin-50");
-});
+// $(document).ready(function () {
+// 	$("h1").css("color", "red");
+// 	$("h1").addClass("big-title margin-50");
+// 	$("h1").removeClass("big-title");
+// 	$("h1").hasClass("margin-50");
+// });
 
 $("h1").text("Bye");
 $("button").html("<strong>Don't</strong> click me");
+
+$("a").attr("href", "https://www.google.com/");
