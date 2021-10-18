7da3a950abe24abfcfcdb1bdde92240c1274adf2
Akash Roshan <akash.second1999@gmail.com>
2021-10-18 10:43:57 +0530

    lecture 166 js objects

---
+++ JS Snippets/objects.js
@@ -0,0 +1,7 @@
+function HouseKeeper(name, yearsOfExperience, cleaningRepertoire) {
+	this.name = name;
+	this.yearsOfExperience = yearsOfExperience;
+	this.cleaningRepertoire = cleaningRepertoire;
+}
+
+var houseKeeper1 = new HouseKeeper("Jane", "5", "Lobby");
