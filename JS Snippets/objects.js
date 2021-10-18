e62a7e1253161634781278a4e117d08492a5412f
Akash Roshan <akash.second1999@gmail.com>
2021-10-18 10:45:21 +0530

    lecture 168 object methods

--- JS Snippets/objects.js
+++ JS Snippets/objects.js
@@ -2,6 +2,10 @@ function HouseKeeper(name, yearsOfExperience, cleaningRepertoire) {
 	this.name = name;
 	this.yearsOfExperience = yearsOfExperience;
 	this.cleaningRepertoire = cleaningRepertoire;
+	this.clean = function () {
+		alert("Cleaning in progress");
+	};
 }
 
 var houseKeeper1 = new HouseKeeper("Jane", "5", "Lobby");
+houseKeeper1.clean();
