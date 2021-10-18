87c3bb31daee2ef27427371caf3680f18d3db537
Akash Roshan <akash.second1999@gmail.com>
2021-10-18 11:36:30 +0530

    lecture 180

---
+++ JQuery/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	// $("h1").css("color", "red");
+	$("h1").addClass("big-title margin-50");
+	// $("h1").removeClass("big-title");
+	// $("h1").hasClass("margin-50");
+});
+
+$("h1").text("Bye");
+$("button").html("<strong>Don't</strong> click me");
