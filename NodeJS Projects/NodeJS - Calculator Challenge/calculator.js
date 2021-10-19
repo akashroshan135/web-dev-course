71fb8ebf31e0b8dc98bc8756527bebf6747cb7f0
Akash Roshan <akash.second1999@gmail.com>
2021-10-19 11:54:12 +0530

    complete section 19

--- NodeJS Projects/NodeJS - Calculator Challenge/calculator.js
+++ NodeJS Projects/NodeJS - Calculator Challenge/calculator.js
@@ -1,11 +1,36 @@
 const express = require("express");
+
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () =>
 	console.log(`App listening at http://localhost:${port}`)
 );
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+	var num1 = Number(req.body.num1);
+	var num2 = Number(req.body.num2);
+
+	var result = num1 + num2;
+
+	res.send(`The result of the calculation is ${result}`);
+});
+
+app.get("/bmicalculator", (req, res) => {
+	res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", (req, res) => {
+	var weight = parseFloat(req.body.weight);
+	var height = parseFloat(req.body.height);
+
+	var bmi = Math.floor(weight / height ** 2);
+
+	res.send(`Your BMI is ${bmi}`);
 });
