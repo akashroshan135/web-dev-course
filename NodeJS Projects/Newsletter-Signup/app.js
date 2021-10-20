0c587e7b829dcdf3147ce430309354ba6c3995e4
Akash Roshan <akash.second1999@gmail.com>
2021-10-20 14:26:49 +0530

    lecture 250 complete site with mailchimp api

--- NodeJS Projects/Newsletter-Signup/app.js
+++ NodeJS Projects/Newsletter-Signup/app.js
@@ -1,5 +1,7 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
 const app = express();
 const port = 3000;
 const views = __dirname + "/views/";
@@ -7,19 +9,47 @@ const views = __dirname + "/views/";
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Setting up mailchimp to add members
+mailchimp.setConfig({
+	apiKey: process.env.API_KEY,
+	server: "us5",
+});
+
 app.get("/", (req, res) => {
 	res.sendFile(views + "/signup.html");
 });
 
 app.post("/", (req, res) => {
-	var firstName = req.body.firstName;
-	var lastName = req.body.lastName;
-	var email = req.body.email;
+	const listId = process.env.LIST_ID;
 
-	console.log(firstName, lastName, email);
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const email = req.body.email;
 
-	// res.sendFile(views + "/success.html");
-	// res.sendFile(views + "/failure.html");
+	// Setting up mailchimp function to add new member
+	async function run() {
+		const response = await mailchimp.lists.addListMember(listId, {
+			email_address: email,
+			status: "subscribed",
+			merge_fields: {
+				FNAME: firstName,
+				LNAME: lastName,
+			},
+		});
+		res.sendFile(views + "/success.html");
+		console.log(
+			`Contact added as an audience member. The contact's id is ${response.id}.`
+		);
+	}
+
+	run().catch((e) => {
+		console.log(e);
+		res.sendFile(views + "/failure.html");
+	});
+});
+
+app.post("/failure", (req, res) => {
+	res.redirect("/");
 });
 
 app.listen(port, () =>
