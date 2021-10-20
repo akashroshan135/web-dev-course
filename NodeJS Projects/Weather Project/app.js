c7733e4ff8a43e4039f3181fead89ee7b618a97e
Akash Roshan <akash.second1999@gmail.com>
2021-10-20 11:55:36 +0530

    lecture 245

--- NodeJS Projects/Weather Project/app.js
+++ NodeJS Projects/Weather Project/app.js
@@ -11,13 +11,23 @@ app.listen(port, () =>
 app.get("/", (req, res) => {
 	const url =
 		"https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=d885b96130728699de8a2dba5f5a47ab&units=metric";
+
 	https.get(url, (response) => {
 		response.on("data", (data) => {
 			const weatherData = JSON.parse(data);
-			console.log(weatherData.weather[0].description);
-			// res.send(weatherData);
+			const temp = weatherData.main.temp;
+			const description = weatherData.weather[0].description;
+			const icon = weatherData.weather[0].icon;
+			const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+			res.write(
+				`<h1>The temparature in Bangalore is ${temp} degrees Celcius.</h1>`
+			);
+			res.write("<br />");
+			res.write(`<h3>The weather is currently ${description}</h3>`);
+			res.write(`<img src=${imageURL} alt="weather image">`);
+
+			res.send();
 		});
 	});
-
-	res.send("Server is running");
 });
