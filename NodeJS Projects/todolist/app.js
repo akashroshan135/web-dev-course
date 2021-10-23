f6e5c500057fcafbca00ea20e46119820fc12937
Akash Roshan <akash.second1999@gmail.com>
2021-10-23 16:58:01 +0530

    lecture 271

--- NodeJS Projects/todolist/app.js
+++ NodeJS Projects/todolist/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -11,15 +12,31 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
 	let today = new Date().toLocaleDateString("en-US", {
 		weekday: "long",
+		day: "numeric",
+		month: "long",
 	});
 
-	res.render("list", { kindOfDay: today, newItems: items });
+	res.render("list", { listTitle: today, Items: items });
+});
+
+app.get("/work", (req, res) => {
+	res.render("list", { listTitle: "Work List", Items: workItems });
 });
 
 app.post("/", (req, res) => {
 	let item = req.body.newItem;
-	items.push(item);
-	res.redirect("/");
+
+	if (req.body.listBtn == "Work List") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
+});
+
+app.get("/about", (req, res) => {
+	res.render("about");
 });
 
 app.listen(port, () =>
