cf749159beab7c19505a7115a3a4560907d045c9
Akash Roshan <akash.second1999@gmail.com>
2021-10-25 20:17:23 +0530

    complete section 27

--- NodeJS Projects/Fruits Project/app.js
+++ NodeJS Projects/Fruits Project/app.js
@@ -20,30 +20,35 @@ const fruitSchema = new mongoose.Schema({
 });
 const Fruit = mongoose.model("Fruits", fruitSchema);
 
-// const fruitItem = new Fruit({
-// 	name: "Apple",
-// 	rating: 8,
-// 	review: "Awesome!",
-// });
+const pineapple = new Fruit({
+	name: "Pineapple",
+	rating: 9,
+	review: "great fruit",
+});
 
-// fruitItem.save();
+pineapple.save((err) => {
+	if (err) return console.error(err);
+	console.log("Document inserted succussfully!");
+});
 
 // Challenge
 const personSchema = new mongoose.Schema({
 	name: String,
 	age: Number,
+	favFruit: fruitSchema,
 });
 const Person = mongoose.model("Persons", personSchema);
 
-// const personItem = new Person({
-// 	name: "Akash",
-// 	age: 8,
-// });
+const personItem = new Person({
+	name: "John",
+	age: 25,
+	favFruit: pineapple,
+});
 
-// personItem.save((err) => {
-// 	if (err) return console.error(err);
-// 	console.log("Document inserted succussfully!");
-// });
+personItem.save((err) => {
+	if (err) return console.error(err);
+	console.log("Document inserted succussfully!");
+});
 
 // const kiwi = new Fruit({
 // 	name: "Kiwi",
@@ -62,37 +67,51 @@ const Person = mongoose.model("Persons", personSchema);
 // 	console.log("Documents inserted succussfully!");
 // });
 
-Fruit.updateOne(
-	{ _id: "6176c013daf137631cdee1d1" },
-	{ name: "Banana" },
+// Fruit.updateOne(
+// 	{ _id: "6176c013daf137631cdee1d1" },
+// 	{ name: "Banana" },
+// 	(err) => {
+// 		if (err) return console.error(err);
+// 		console.log("Updated data successfully");
+// 	}
+// );
+
+// Fruit.deleteOne({ name: "Banana" }, (err) => {
+// 	if (err) return console.error(err);
+// 	console.log("Deleted data successfully");
+// });
+
+// Fruit.find((err, fruits) => {
+// 	if (err) return console.error(err);
+// 	fruits.forEach((fruit) => {
+// 		console.log(fruit.name);
+// 	});
+// });
+
+// Person.deleteMany({ name: "Akash" }, (err) => {
+// 	if (err) return console.error(err);
+// 	console.log("Deleted data successfully");
+// });
+
+// Person.find((err, persons) => {
+// 	if (err) return console.error(err);
+// 	persons.forEach((person) => {
+// 		console.log(person.name);
+// 	});
+// });
+
+Person.updateOne(
+	{ _id: "6176b9ac161e15d2d9d2c43f" },
+	{ favFruit: pineapple },
 	(err) => {
 		if (err) return console.error(err);
 		console.log("Updated data successfully");
 	}
 );
 
-Fruit.deleteOne({ name: "Banana" }, (err) => {
-	if (err) return console.error(err);
-	console.log("Deleted data successfully");
-});
-
-Fruit.find((err, fruits) => {
-	if (err) return console.error(err);
-	fruits.forEach((fruit) => {
-		console.log(fruit.name);
-	});
-});
-
-Person.deleteMany({ name: "Akash" }, (err) => {
-	if (err) return console.error(err);
-	console.log("Deleted data successfully");
-});
-
 Person.find((err, persons) => {
 	if (err) return console.error(err);
-	persons.forEach((person) => {
-		console.log(person.name);
-	});
+	console.log(persons);
 });
 
 // Allows node to terminate app using ctrl + c
