ef9ed9d058b56b8d6cb2ee8f20abad8bb0f21c5c
Akash Roshan <akash.second1999@gmail.com>
2021-10-25 19:52:24 +0530

    lecture 338

--- NodeJS Projects/Fruits Project/app.js
+++ NodeJS Projects/Fruits Project/app.js
@@ -7,63 +7,69 @@ async function main() {
 }
 
 const fruitSchema = new mongoose.Schema({
-	name: String,
-	rating: Number,
+	name: {
+		type: String,
+		required: [true, "Name field is empty"],
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 10,
+	},
 	review: String,
 });
 const Fruit = mongoose.model("Fruits", fruitSchema);
 
 const fruitItem = new Fruit({
-	name: "Mango",
-	rating: 8,
-	review: "Great!",
+	name: "Apple",
+	rating: 5,
+	review: "Awesome!",
 });
 
-// fruitItem.save();
+fruitItem.save();
 
-// Challenge
-const personSchema = new mongoose.Schema({
-	name: String,
-	age: Number,
-});
-const Person = mongoose.model("Persons", personSchema);
+// // Challenge
+// const personSchema = new mongoose.Schema({
+// 	name: String,
+// 	age: Number,
+// });
+// const Person = mongoose.model("Persons", personSchema);
 
-const personItem = new Person({
-	name: "Akash",
-	age: 8,
-});
+// const personItem = new Person({
+// 	name: "Akash",
+// 	age: 8,
+// });
 
 // personItem.save((err) => {
 // 	if (err) return console.error(err);
 // 	console.log("Document inserted succussfully!");
 // });
 
-const kiwi = new Fruit({
-	name: "Kiwi",
-	rating: 9,
-	review: "Great!",
-});
+// const kiwi = new Fruit({
+// 	name: "Kiwi",
+// 	rating: 9,
+// 	review: "Great!",
+// });
 
-const orange = new Fruit({
-	name: "Orange",
-	rating: 7,
-	review: "Great!",
-});
+// const orange = new Fruit({
+// 	name: "Orange",
+// 	rating: 7,
+// 	review: "Great!",
+// });
 
 // Fruit.insertMany([kiwi, orange], (err) => {
 // 	if (err) return console.error(err);
 // 	console.log("Documents inserted succussfully!");
 // });
 
-Fruit.find((err, fruits) => {
-	if (err) return console.error(err);
-	fruits.forEach((fruit) => {
-		console.log(fruit.name);
-	});
-});
+// Fruit.find((err, fruits) => {
+// 	if (err) return console.error(err);
+// 	fruits.forEach((fruit) => {
+// 		console.log(fruit.name);
+// 	});
+// });
 
 // Allows node to terminate app using ctrl + c
 process.on("SIGINT", function () {
-	console.log("Shutting down app");
 	process.exit(0);
 });
