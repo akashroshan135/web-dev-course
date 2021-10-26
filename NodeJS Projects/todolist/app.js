105e630ec9aa923fb284a783e166f7aa5283cfe5
Akash Roshan <akash.second1999@gmail.com>
2021-10-26 17:29:48 +0530

    lecture 345

--- NodeJS Projects/todolist/app.js
+++ NodeJS Projects/todolist/app.js
@@ -1,18 +1,56 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
 const port = 3000;
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// connect Mongoose
+main().catch((err) => console.log(err));
+async function main() {
+	await mongoose.connect("mongodb://localhost:27017/todoDB");
+}
+
+const tasksSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Task field is empty"],
+	},
+});
+const Task = mongoose.model("Task", tasksSchema);
+
+function addDefaultItems() {
+	const task1 = new Task({
+		name: "Welcome to you todo list!",
+	});
+	const task2 = new Task({
+		name: "Hit the + button to add a new item",
+	});
+	const task3 = new Task({
+		name: "<-- Hit this to mark as done",
+	});
+	const defaultTasks = [task1, task2, task3];
+	Task.insertMany(defaultTasks, (err) => {
+		if (err) return console.log(err);
+		console.log("Default items inserted");
+	});
+}
+
 app.get("/", (req, res) => {
 	const today = date.getDate();
-	res.render("list", { listTitle: today, Items: items });
+	Task.find((err, tasks) => {
+		if (err) return console.error(err);
+		if (tasks.length === 0) {
+			addDefaultItems();
+			res.redirect("/");
+		} else {
+			res.render("list", { listTitle: today, Items: tasks });
+		}
+	});
 });
 
 app.get("/work", (req, res) => {
@@ -23,10 +61,29 @@ app.post("/", (req, res) => {
 	const item = req.body.newItem;
 
 	if (req.body.listBtn == "Work List") {
-		workItems.push(item);
+		// workItems.push(item);
 		res.redirect("/work");
 	} else {
-		items.push(item);
+		const taskItem = new Task({ name: item });
+		taskItem.save((err) => {
+			if (err) return console.error(err);
+			console.log("Task Added Successfully");
+		});
+		res.redirect("/");
+	}
+});
+
+app.post("/checked", (req, res) => {
+	const item = req.body.checkbox;
+
+	if (req.body.listBtn == "Work List") {
+		// workItems.push(item);
+		res.redirect("/work");
+	} else {
+		Task.findOneAndDelete(item, (err) => {
+			if (err) return console.error(err);
+			console.log("Deleted Item Successfully");
+		});
 		res.redirect("/");
 	}
 });
