c9896dc9bb9b3f5fa5b0ddeec0a64665a0023a7d
Akash Roshan <akash.second1999@gmail.com>
2021-11-01 19:21:39 +0530

    lecture 428

---
+++ ReactJS/changing-complex-state-practice/src/components/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setContact(() => {
+      if (name === "fName")
+        return {
+          ...contact,
+          fName: value,
+        };
+      if (name === "lName")
+        return {
+          ...contact,
+          lName: value,
+        };
+      if (name === "email")
+        return {
+          ...contact,
+          email: value,
+        };
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" onChange={handleChange} />
+        <input name="lName" placeholder="Last Name" onChange={handleChange} />
+        <input name="email" placeholder="Email" onChange={handleChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
