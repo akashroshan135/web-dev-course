db3f27fbe1ba069fc5f6bdfd56029d2ee317ed18
Akash Roshan <akash.second1999@gmail.com>
2021-11-01 15:06:31 +0530

    lecture 416

--- ReactJS/keeper/src/components/App.jsx
+++ ReactJS/keeper/src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note {...note} />
+      ))}
       <Footer />
     </div>
   );
